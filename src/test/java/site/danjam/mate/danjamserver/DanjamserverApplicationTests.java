64e3ed7d04420f1fcff67c800888c4147f35fd08
ryudain05 <dkdlsekdls@naver.com>
2024-12-19 10:15:45 +0900

    [init] Init danjam Project

---
+++ src/test/java/site/danjam/mate/danjamserver/DanjamserverApplicationTests.java
@@ -0,0 +1,13 @@
+package site.danjam.mate.danjamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanjamserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
