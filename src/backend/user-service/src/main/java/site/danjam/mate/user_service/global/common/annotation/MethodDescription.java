3f4a1f46a42b04e0478928622fb91d808ee781dc
ryudain05 <dkdlsekdls@naver.com>
2024-12-19 15:32:47 +0900

    [init] GlobalException, Code, annotation 추가
    [목적] 프로젝트 공통 사항을 위해 exception, code, annotation 추가
    
    [목표] 기존 단잠의 공통 형식 이관

---
+++ src/backend/user-service/src/main/java/site/danjam/mate/user_service/global/common/annotation/MethodDescription.java
@@ -0,0 +1,12 @@
+package site.danjam.mate.user_service.global.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodDescription {
+    String description() default "";
+}
