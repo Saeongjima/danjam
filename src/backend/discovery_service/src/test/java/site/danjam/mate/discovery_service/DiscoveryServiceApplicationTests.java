64dfb9838db0b78859ee93910947e52e9636ffdc
Your Name <kim946509@gmail.com>
2025-01-02 20:02:32 +0900

    [fix]: Setting discovery-service repeat
    [목적]: Eureka server를 생성하기 위하여 discovery-service 프로젝트 생성 및 작성후 패키지명 및 기타 요소들을 통일 화 하기 위해 다시 생성
    
    [목표]: 초기 Discovery server 셋팅.
    
    [달성도]:
    
      - Discovery service 코드 초기 셋팅 작성
    
    [기타]:
    - 기존 user-service에 의존성을 추가하여 micro-service로 변환 예정
    - api-gateway 서버 코드 작성 예정

--- src/backend/discovery_service/src/test/java/site/danjam/mate/discovery_service/DiscoveryServiceApplicationTests.java
+++ src/backend/discovery_service/src/test/java/site/danjam/mate/discovery_service/DiscoveryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sequence.discoveryservice;
+package site.danjam.mate.discovery_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
