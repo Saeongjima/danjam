e261c536cd0298f39f7be522f614e64560ebddcb
ryudain05 <dkdlsekdls@naver.com>
2025-01-24 15:17:44 +0900

    [init] chat-service init [목적] 채팅 개발을 위한 chat-service 프로젝트 셋팅

---
+++ src/backend/chat-serivce/src/test/java/site/danjam/mate/chat_serivce/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package site.danjam.mate.chat_serivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
