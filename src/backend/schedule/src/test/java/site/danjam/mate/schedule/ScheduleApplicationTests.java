bdb899bbe8e06e7b43fd3ca1e6aedf60cda29c1f
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2025-01-28 03:19:54 +0900

    first commit
    - gitignore 수정
    - application.properties 설정
    - docker-compose 설정

---
+++ src/backend/schedule/src/test/java/site/danjam/mate/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package site.danjam.mate.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
