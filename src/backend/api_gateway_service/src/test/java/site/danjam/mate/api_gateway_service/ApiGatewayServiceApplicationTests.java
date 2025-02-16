f65a99c45b527a93a5a02eeea42e667658b5355e
Your Name <kim946509@gmail.com>
2025-02-16 22:21:44 +0900

    fix : application.yml 환경변수 수정
    목적 : 환경변수가 환경변수 파일과 일치시키도록 하기 위해 변경함.

--- src/backend/api_gateway_service/src/test/java/site/danjam/mate/api_gateway_service/ApiGatewayServiceApplicationTests.java
+++ src/backend/api_gateway_service/src/test/java/site/danjam/mate/api_gateway_service/ApiGatewayServiceApplicationTests.java
@@ -2,8 +2,10 @@ package site.danjam.mate.api_gateway_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:properties/env.properties")
 class ApiGatewayServiceApplicationTests {
 
     @Test
