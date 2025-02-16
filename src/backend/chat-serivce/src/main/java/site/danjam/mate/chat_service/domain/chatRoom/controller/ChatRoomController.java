4c6621ca27b9cdab0715cf46371dc561e8659a75
ryudain05 <dkdlsekdls@naver.com>
2025-02-16 20:49:26 +0900

    [feat] 유저 채팅방 조회 컨트롤러 추가 [목적] 유저 채팅방 조회 컨트롤러 추가 [달성도] 80 todo: 채팅방 조회 테스트 추가 예정

--- src/backend/chat-serivce/src/main/java/site/danjam/mate/chat_service/domain/chatRoom/controller/ChatRoomController.java
+++ src/backend/chat-serivce/src/main/java/site/danjam/mate/chat_service/domain/chatRoom/controller/ChatRoomController.java
@@ -1,16 +1,20 @@
 package site.danjam.mate.chat_service.domain.chatRoom.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.danjam.mate.chat_service.domain.chatRoom.dto.ChatRoomListDTO;
 import site.danjam.mate.chat_service.domain.chatRoom.dto.GroupChatRoomCreateDTO;
 import site.danjam.mate.chat_service.domain.chatRoom.dto.PersonalChatRoomCreateDTO;
 import site.danjam.mate.chat_service.domain.chatRoom.dto.request.GroupChatRoomRequestDTO;
 import site.danjam.mate.chat_service.domain.chatRoom.service.ChatRoomCommandService;
+import site.danjam.mate.chat_service.domain.chatRoom.service.ChatRoomQueryService;
 import site.danjam.mate.chat_service.global.common.dto.ApiResponseData;
 import site.danjam.mate.chat_service.global.common.enums.Role;
 
@@ -19,6 +23,7 @@ import site.danjam.mate.chat_service.global.common.enums.Role;
 @RequestMapping("chat-service/api/chat-room")
 public class ChatRoomController {
     private final ChatRoomCommandService chatRoomCommandService;
+    private final ChatRoomQueryService chatRoomQueryService;
 
     @PostMapping("/personal")
     public ResponseEntity<ApiResponseData<PersonalChatRoomCreateDTO>> createPersonalChatRoom(
@@ -34,5 +39,11 @@ public class ChatRoomController {
             @RequestBody GroupChatRoomRequestDTO dto) {
         return ResponseEntity.ok(ApiResponseData.of(chatRoomCommandService.createGroupChatRoom(userId, role, dto),
                 "채팅방이 생성되었습니다."));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponseData<List<ChatRoomListDTO>>> leaveChatRoom(
+            @RequestHeader("userId") String userId) {
+        return ResponseEntity.ok(ApiResponseData.of(chatRoomQueryService.getUserChatRoom(userId), "채팅방 목록 조회 성공"));
     }
 }
