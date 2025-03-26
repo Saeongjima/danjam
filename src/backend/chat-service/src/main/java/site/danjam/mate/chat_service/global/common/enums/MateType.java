ac982ba9e5618678f34d08e84fad55d6ec93da89
ryudain05 <dkdlsekdls@naver.com>
2025-03-26 21:18:34 +0900

    [fix] enums jsonValue 추가 [목적] 한글 처리를 위해 jsonValue 추가, 추후 따로 메서드로 뺄 여지 있음

--- src/backend/chat-service/src/main/java/site/danjam/mate/chat_service/global/common/enums/MateType.java
+++ src/backend/chat-service/src/main/java/site/danjam/mate/chat_service/global/common/enums/MateType.java
@@ -1,6 +1,7 @@
 package site.danjam.mate.chat_service.global.common.enums;
 
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 public enum MateType {
     FOODMATE("식사메이트"), ROOMMATE("룸메이트"), WORKOUTMATE("운동메이트"),
     WALKMATE("산책메이트"), STUDYMATE("스터디메이트");
+
+
+    @JsonValue
     private final String mateType;
 }
 
