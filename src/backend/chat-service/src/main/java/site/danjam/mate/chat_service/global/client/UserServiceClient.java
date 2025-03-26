57abd44443ab40bd3825522b2598ff5be59fd55f
ryudain05 <dkdlsekdls@naver.com>
2025-03-27 00:16:49 +0900

    [refactor] UserServiceClient Url Fix [목적] PathVariable 로 URL 명시

--- src/backend/chat-service/src/main/java/site/danjam/mate/chat_service/global/client/UserServiceClient.java
+++ src/backend/chat-service/src/main/java/site/danjam/mate/chat_service/global/client/UserServiceClient.java
@@ -3,18 +3,19 @@ package site.danjam.mate.chat_service.global.client;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import site.danjam.mate.chat_service.global.client.dto.UserInfoDTO;
 
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
-    @GetMapping("/user-service/api/user/inquiry/info")
-    UserInfoDTO getUserInfoById(@RequestParam("userId") Long userId);
+    @GetMapping("/user-service/api/user/inquiry/info/{id}")
+    UserInfoDTO getUserInfoById(@PathVariable("id") Long userId);
 
-    @GetMapping("/user-service/api/user/inquiry/info")
-    UserInfoDTO getUserInfoByUsername(@RequestParam("username") String username);
+    @GetMapping("/user-service/api/user/inquiry/info/{username}")
+    UserInfoDTO getUserInfoByUsername(@PathVariable("username") String username);
 
-    @GetMapping("/user-service/api/user/inquiry/info")
+    @GetMapping("/user-service/api/user/inquiry/info/ids")
     List<UserInfoDTO> getUserInfoByIds(@RequestParam("userIds") List<Long> userIds);
 }
 
