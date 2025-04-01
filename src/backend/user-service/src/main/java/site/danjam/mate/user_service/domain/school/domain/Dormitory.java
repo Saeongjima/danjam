2bf96bf705e45f1c31a024ed1d66213538961aa0
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-04-02 00:52:50 +0900

    refactor : 기숙사 인실정보 테이블 추가
    - 각 기숙사마다 2인실 4인실등 존재하는 호실 정보를 당므.

--- src/backend/user-service/src/main/java/site/danjam/mate/user_service/domain/school/domain/Dormitory.java
+++ src/backend/user-service/src/main/java/site/danjam/mate/user_service/domain/school/domain/Dormitory.java
@@ -3,40 +3,36 @@ package site.danjam.mate.user_service.domain.school.domain;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import site.danjam.mate.common.utils.BaseTimeEntity;
 
 @Entity
 @Getter
+@Table(name = "dormitory")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "school")
-public class School extends BaseTimeEntity {
+public class Dormitory {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
-    private String name;
-    @Column(nullable = false)
-    private String korName;
+    private String dormitoryName;
 
-    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL)
-    private List<College> collegeList;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "school_id", nullable = false)
+    private School school;
 
-    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL)
-    private List<Dormitory> dormitoryList;
-
-    @Builder
-    public School(String name, String korName) {
-        this.name = name;
-        this.korName = korName;
-    }
+    @OneToMany(mappedBy = "dormitory", cascade = CascadeType.ALL)
+    private List<DormitoryRoom> dormitoryRoomList;
 }
--- src/backend/user-service/src/main/java/site/danjam/mate/user_service/domain/school/domain/DormitoryRoom.java
+++ src/backend/user-service/src/main/java/site/danjam/mate/user_service/domain/school/domain/DormitoryRoom.java
@@ -1,6 +1,5 @@
 package site.danjam.mate.user_service.domain.school.domain;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,30 +8,25 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "college")
+@Table(name = "dormitory_room")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class College {
+public class DormitoryRoom {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private String collegeName;
+    private Integer capacity;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "school_id", nullable = false)
-    private School school;
-
-    @OneToMany(mappedBy = "college", cascade = CascadeType.ALL)
-    private List<Major> majorList;
+    @JoinColumn(name = "dormitory_id", nullable = false)
+    private Dormitory dormitory;
 }
